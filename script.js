164659bc6cef4c4f2d27242672d9d899f7829d7e
Tanja Vidic <vidict44@gmail.comgit config --global user.name Tanja>
2019-10-21 14:30:51 -0700

    finished the last part: json

--- script.js
+++ script.js
@@ -47,17 +47,23 @@ window.addEventListener("load", function() {
    });
 
 
-  
-
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+//This block of code shows how to format the HTML once you fetch some planetary JSON!
+window.addEventListener("load", function() {
+            fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+               response.json().then( function(json) {
+                  const div = document.getElementById("missionTarget");
+                  // Add HTML that includes the JSON data
+                  div.innerHTML = `
+                    <h2>Mission Destination</h2>
+                     <ol>
+                        <li>Name: ${json[0].name}</li>
+                        <li>Diameter: ${json[0].diameter}</li>
+                        <li>Star: ${json[0].star}</li>
+                        <li>Distance from Earth: ${json[0].distance}</li>
+                        <li>Number of Moons: ${json[0].moons}</li>
+                     </ol>
+                        <img src="${json[0].image}">
+                  `;
+               });
+            });
+         });
